25478dc6ce8b3a083e40714625630a8667143869
William Russell <williamrussell@Williams-MBP.lan>
2023-02-05 22:36:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch ('https://anapioficeandfire.com/api/books')
+  .then (res => res.json())
+  .then (json => renderBooks(json))
+  return fetch ('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(books) {
